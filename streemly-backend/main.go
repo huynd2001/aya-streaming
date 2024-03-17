de9e04171196ffbe0baf5937eb454af7a6fb64b5
Huy Nguyen <ndhuy01@gmail.com>
2024-03-17 16:29:59 -0400

    feat: POC with a test emitter

--- streemly-backend/main.go
+++ streemly-backend/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	testsource "streemly-backend/service/testsource"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+	testEmitter := testsource.NewEmitter()
+
+	for {
+		newMessage := <-testEmitter.UpdateEmitter()
+		fmt.Println("{}", newMessage)
+	}
 }
