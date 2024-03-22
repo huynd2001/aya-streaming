af12458540bdfbdd51369ab71a87845eac24280f
Huy Nguyen <ndhuy01@gmail.com>
2024-03-21 21:27:50 -0400

    feat: react stuff

--- aya-frontend/src/app/home/page.tsx
+++ aya-frontend/src/app/home/page.tsx
@@ -2,11 +2,20 @@
 
 import { Alignment, Button, Navbar } from "@blueprintjs/core";
 
+import { useSearchParams } from "next/navigation";
+
 import "normalize.css/normalize.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import { useEffect } from "react";
 
 export default function Page() {
+  const searchParams = useSearchParams();
+  // console.log(params);
+  useEffect(() => {
+    const id_token = searchParams.get("id_token");
+    console.log(id_token);
+  }, [searchParams]);
   return (
     <Navbar>
       <Navbar.Group align={Alignment.LEFT}>
