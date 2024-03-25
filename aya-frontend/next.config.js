20686de9678ffd3acf5105cd0b301c0a00097a72
Huy Nguyen <ndhuy01@gmail.com>
2024-03-24 21:26:14 -0400

    feat: moving everything to layout to reuse

--- aya-frontend/next.config.js
+++ aya-frontend/next.config.js
@@ -9,4 +9,13 @@ module.exports = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
 };
