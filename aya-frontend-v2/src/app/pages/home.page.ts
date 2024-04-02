82ef910ee1d239902f40087f636b3261cb401f99
Huy Nguyen <ndhuy01@gmail.com>
2024-04-02 15:26:30 -0400

    feat: migrate to @if directive

--- aya-frontend-v2/src/app/pages/home.page.ts
+++ aya-frontend-v2/src/app/pages/home.page.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  template: ` <p>home page works!!</p> `,
+  imports: [MatToolbar, MatIcon, MatIconButton],
+  templateUrl: 'home.page.html',
+  styleUrl: 'home.page.css',
 })
 export default class HomePage {}
