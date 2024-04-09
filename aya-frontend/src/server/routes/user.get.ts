7b76032c4c53792c7ac2a5c701e56e9649b2f9da
Nguyen Duc Huy <ndhuy01@gmail.com>
2024-04-09 13:15:05 -0400

    feat: user info retrieval (#3)

---
+++ aya-frontend/src/server/routes/user.get.ts
@@ -0,0 +1,31 @@
+import { defineEventHandler, getQuery, getRequestHeader } from 'h3';
+
+import axios from 'axios';
+import { constants } from '../../app/interfaces/constants';
+
+const endPoint = import.meta.env[constants.API_URL_ENV];
+
+export default defineEventHandler(async (event) => {
+  const params = getQuery(event);
+  const authorization = getRequestHeader(event, 'Authorization');
+
+  try {
+    let { data } = await axios.get(`${endPoint}/user/`, {
+      headers: {
+        Authorization: authorization,
+      },
+      params: params,
+    });
+    return data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      if (err.response) {
+        console.error(err.response.status);
+        return err.response.data;
+      }
+    } else {
+      console.error(err);
+      return { err: err };
+    }
+  }
+});
