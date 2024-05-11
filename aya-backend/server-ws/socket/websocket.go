5ecae971f1fad532156ebac52ddae8e4709190b8
Huy Nguyen <ndhuy01@gmail.com>
2024-05-10 22:03:35 -0400

    feat: lint

--- aya-backend/server-ws/socket/websocket.go
+++ aya-backend/server-ws/socket/websocket.go
@@ -11,7 +11,6 @@ import (
 	"net/http"
 	"os"
 	"sync"
-	"unicode/utf8"
 )
 
 const (
@@ -128,28 +127,6 @@ func wsHandler(wsServer *WSServer) http.HandlerFunc {
 		wsServer.mutex.Unlock()
 		fmt.Printf("Session %s is disconnected\n", sessionUUID)
 	}
-}
-
-func equalASCIIFold(s, t string) bool {
-	for s != "" && t != "" {
-		sr, size := utf8.DecodeRuneInString(s)
-		s = s[size:]
-		tr, size := utf8.DecodeRuneInString(t)
-		t = t[size:]
-		if sr == tr {
-			continue
-		}
-		if 'A' <= sr && sr <= 'Z' {
-			sr = sr + 'a' - 'A'
-		}
-		if 'A' <= tr && tr <= 'Z' {
-			tr = tr + 'a' - 'A'
-		}
-		if sr != tr {
-			return false
-		}
-	}
-	return s == t
 }
 
 func NewWSServer(
